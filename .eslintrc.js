2ab872818c887708f06125e783e6c714621c39c0
SH2068 <dleldjtm@naver.com>
2024-09-05 11:18:56 +0900

    out

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,21 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  extends: ["plugin:react/recommended", "standard-with-typescript", "plugin:@next/next/recommended", "prettier"],
   overrides: [],
   parserOptions: {
     project: "**/tsconfig.json",
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ["react", "@next/eslint-plugin-next"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-misused-promises": "off", // HOF를 배우고 나서 wrapAsync 함수로 해결 가능(그 전까지는 off 할 것)
+  },
+settings: {
+    react: {
+      version: "detect", // React 버전 자동 감지
+    },
   },
 };
